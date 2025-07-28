9e1ba3836fea8b0c3a86df1ac66dbd77afada8ff
Girish2005-gm <gmaheshwari799@gmail.com>
2025-07-28 20:04:36 +0530

    Added SEO metadata and deleted unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import Loading from "@/components/loading"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Girish Kumar - Full Stack Developer | Software Engineer ",
-  description: "Portfolio website of Girish Kumar, a Full Stack Developer.",
+  title: "Girish Kumar - Full Stack Developer | Software Engineer",
+  description: "Portfolio website of Girish Kumar, showcasing full-stack projects and skills.",
   icons: {
     icon: [
       { url: '/coder-in-hoodie-logo.png', sizes: 'any', type: 'image/x-icon' },
@@ -20,7 +20,30 @@ export const metadata: Metadata = {
     ],
     apple: '/coder-in-hoodie-logo.png',
   },
+  openGraph: {
+    title: "Girish Kumar - Full Stack Developer",
+    description: "Portfolio website of Girish Kumar, showcasing full-stack projects and skills.",
+    url: "https://portfolio-girish-kumar.vercel.app",
+    siteName: "Girish Kumar Portfolio",
+    images: [
+      {
+        url: "/og-image.png", // Yeh image tu public folder mein daal
+        width: 1200,
+        height: 630,
+        alt: "Girish Portfolio OG Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Girish Kumar - Full Stack Developer",
+    description: "Explore projects and experience of Girish Kumar, a skilled React and Node developer.",
+    images: ["/og-image.png"],
+  },
 }
+
 
 export default function RootLayout({
   children,
